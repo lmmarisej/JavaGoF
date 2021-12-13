326b76b1739f9b2db3212e6fdd0471eecc1420a0
lmmarisej <lmmarise@foxmail.com>
2021-12-13 23:26:11 +0800

    Sample

---
+++ p26_GuardedSuspension/src/Sample/RequestQueue.java
@@ -0,0 +1,23 @@
+package Sample;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RequestQueue {
+    private final Queue<Request> queue = new LinkedList<>();
+
+    public synchronized Request getRequest() {
+        while (queue.peek() == null) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
+        return queue.remove();
+    }
+
+    public synchronized void putRequest(Request request) {
+        queue.offer(request);
+        notifyAll();
+    }
+}
