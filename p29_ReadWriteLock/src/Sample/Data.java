ce6220d84a12ecf566ae6696c96b39deaf1dc1f9
lmmarisej <lmmarise@foxmail.com>
2021-12-14 16:55:59 +0800

    add 读写分离锁

---
+++ p29_ReadWriteLock/src/Sample/Data.java
@@ -0,0 +1,52 @@
+package Sample;
+
+import java.util.Arrays;
+
+public class Data {
+    private final char[] buffer;
+    private final ReadWriteLock lock = new ReadWriteLock();
+
+    public Data(int size) {
+        this.buffer = new char[size];
+        Arrays.fill(buffer, '*');
+    }
+
+    public char[] read() throws InterruptedException {
+        lock.readLock();
+        try {
+            return doRead();
+        } finally {
+            lock.readUnlock();
+        }
+    }
+
+    public void write(char c) throws InterruptedException {
+        lock.writeLock();
+        try {
+            doWrite(c);
+        } finally {
+            lock.writeUnlock();
+        }
+    }
+
+    private char[] doRead() {
+        char[] newbuf = new char[buffer.length];
+        System.arraycopy(buffer, 0, newbuf, 0, buffer.length);
+        slowly();
+        return newbuf;
+    }
+
+    private void doWrite(char c) {
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] = c;
+            slowly();
+        }
+    }
+
+    private void slowly() {
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
